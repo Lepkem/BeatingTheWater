54e17c760bd83344d4ab0d0eea1d5fdbac1e0dc6
Alexander <alexandervvugt@gmail.com>
2021-06-15 16:10:12 +0200

    small bugfixes

--- duinen/backend/renderer.py
+++ duinen/backend/renderer.py
@@ -8,32 +8,39 @@ sys.path.append('C:\\OSGeo4W64\\apps\\qgis\\python\\plugins')
 import processing
 
 def render(image: ImageSingleton.Image, source_filepath: str):
-    target_path = os.path.join(os.path.dirname(__file__), '\output.tif')
+    target_path = os.path.join(os.path.dirname(__file__), 'output.tif')
     #TODO: Check if gtif driver exists
     driver_tiff = gdal.GetDriverByName("GTiff")
+    rlayer = QgsRasterLayer(source_filepath, "sourcefile")
+    xmin = rlayer.extent().xMinimum()
+    ymin = rlayer.extent().yMinimum()
+    xoffs, yoffs = find_offset_xy(xmin, ymin, image.x_min, image.y_min, ImageSingleton.Image.resolution)
+    xmaxoffs, ymaxoffs = find_offset_xy(xmin, ymin, image.x_max, image.y_max, ImageSingleton.Image.resolution)
     #TODO: Why float32?
-    renderlayer = driver_tiff.Create(target_path, xsize=int(image.x_max), ysize=int(image.y_max), bands=1, eType=gdal.GDT_Float32)
+    renderlayer = driver_tiff.Create(target_path, xsize=xmaxoffs, ysize=ymaxoffs, bands=1, eType=gdal.GDT_Float32)
     sf = gdal.Open(source_filepath)
     renderlayer.SetGeoTransform(sf.GetGeoTransform())
     renderlayer.SetProjection(sf.GetProjection())
     pixelband = renderlayer.GetRasterBand(1).ReadAsArray()
-    with QgsRasterLayer(source_filepath, "sourcefile") as rlayer:
-        xmin = rlayer.extent().xMinimum()
-        ymin = rlayer.extent().yMinimum()
-    xoffs, yoffs = find_offset_xy(xmin, ymin, image.x_min, image.y_min, ImageSingleton.Image.resolution)
-    xmaxoffs, ymaxoffs = find_offset_xy(xmin, ymin, image.x_max, image.y_max)
+    
     xpointer = image.x_min
     ypointer = image.y_min
     for column in range(xoffs, xmaxoffs):
         for row in range(yoffs, ymaxoffs):
-            pixelband[row][column] = image.seek(xpointer, ypointer)._rating
+            #rating = image.seek(xpointer, ypointer)._rating.value
+            pixelband[row][column] = image.seek(xpointer, ypointer)._rating.value
             ypointer += ImageSingleton.Image.resolution
         xpointer += ImageSingleton.Image.resolution
         ypointer = image.y_min
     renderlayer.GetRasterBand(1).WriteArray(pixelband)
     #TODO: render colors
-    color_conf = os.path.join(os.path.dirname(__file__), '..\color_conf.txt')
-    processing.run('gdal:colorrelief', {'INPUT': renderlayer,
+    color_conf = os.path.join(os.path.dirname(__file__), 'color_conf.txt')
+    from qgis.analysis import QgsNativeAlgorithms
+    QgsApplication.processingRegistry().addProvider(QgsNativeAlgorithms())
+    prints = list()
+    for alg in QgsApplication.processingRegistry().algorithms():
+        prints.append(alg.id() + "->" + alg.displayName())
+    processing.run("gdal:colorrelief", {'INPUT': renderlayer,
         'BAND': 1,
         'COMPUTE_EDGES': False,
         'COLOR_TABLE': color_conf,
