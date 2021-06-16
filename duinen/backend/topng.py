36d599256cbe3d2d53a0007f7d1e7db48a4df3ef
GerbenPut <gerbenputprivate@gmail.com>
2021-06-17 00:04:53 +0200

    hopefully fixes

--- duinen/backend/topng.py
+++ duinen/backend/topng.py
@@ -19,23 +19,23 @@ def convert(request):
             # Passing the data to template (The data below here should later be replaced by the algoritm's output: a tiff image and download button)
             tiffImage = Image.open(tifffile)
             tiffImage = tiffImage.convert('RGB')
-            tiffImage.save("./static/outputdata/tiffToPng.png", "PNG")
+            tiffImage.save("static/outputdata/tiffToPng.png", "PNG")
             # raise Exception("Sorry, no numbers below zero")
 
 def removeoldpng():
-    if os.path.exists("./static/outputdata/tiffToPng.png"):
-        os.remove("./static/outputdata/tiffToPng.png")
+    if os.path.exists("static/outputdata/tiffToPng.png"):
+        os.remove("static/outputdata/tiffToPng.png")
 
 def removefiles():
-    if os.path.exists("./static/outputdata/converted.tif"):
-        os.remove("./static/outputdata/converted.tif")
-    if os.path.exists("./static/outputdata/converted.png"):
-        os.remove("./static/outputdata/converted.png")
+    if os.path.exists("static/outputdata/converted.tif"):
+        os.remove("static/outputdata/converted.tif")
+    if os.path.exists("static/outputdata/converted.png"):
+        os.remove("static/outputdata/converted.png")
 
 def cout():
 
     # Passing the data to template (The data below here should later be replaced by the algoritm's output: a tiff image and download button)
     tiffImage = Image.open(os.path.join(os.path.dirname(__file__), 'output2.tif'))
     tiffImage = tiffImage.convert('RGB')
-    tiffImage.save("./static/outputdata/converted.png", "PNG")
+    tiffImage.save("static/outputdata/converted.png", "PNG")
     # raise Exception("Sorry, no numbers below zero")
